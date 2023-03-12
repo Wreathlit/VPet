656c7a59f64f6dbbf9c3570f14c531fdb86a96bf
Wreathlit <loli.eliya@gmail.com>
2023-03-12 23:37:41 +0800

    修复随机播放机制，现在动画连接时会优先选用上一个动画的随机结果

---
+++ VPet-Simulator.Core/New/AnimationControllerHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using static VPet_Simulator.Core.GraphCore;
+
+namespace VPet_Simulator.Core
+{
+    public static class AnimationControllerHelper
+    {
+        public static string GetGrpahString(this GraphType graphType)
+        {
+            return graphType.ToString().Replace("_Start", "").Replace("_Loop", "").Replace("_End", "").ToLower();
+        }
+
+        static readonly string[] existSegment = { "_A(?=_|$)", "_B(?=_|$)", "_C(?=_|$)" };
+        public static bool IsAnimationFamily(string a, string b)
+        {
+            foreach (var s in existSegment)
+            {
+                a = Regex.Replace(a, s, "", RegexOptions.IgnoreCase);
+                b = Regex.Replace(b, s, "", RegexOptions.IgnoreCase);
+            }
+            return a == b;
+        }
+    }
+}
